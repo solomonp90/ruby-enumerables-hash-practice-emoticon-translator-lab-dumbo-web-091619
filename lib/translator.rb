4e43f1f1aee8893ac0d2ebf5a106c5b3edcd367d
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 23:20:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,23 +1,15 @@
 require "yaml"
 
-# def load_library(file)
-# hash = {"get_meaning"=>{},"get_emoticon"=>{}}
-#   YAML.load_file(file).each do |char_meaning,list|
-#      english,japanese = list
-#     hash[get_emoticon][english] = japanese
-#     hash[get_meaning][japanese] = char_meaning
-#  end
-#  hash
-# end
 def load_library(file)
-  # code goes here	  hash = {"get_meaning" => {}, "get_emoticon" => {}}
-  YAML.load_file('file').each do |char_meaning, list|
-    eng, japn = list
-    hash["get_emoticon"][eng] = japn
-    hash["get_meaning"][japn] = char_meaning
-  end
-  hash
+hash = {"get_meaning"=>{},"get_emoticon"=>{}}
+  YAML.load_file(file).each do |meaning,list|
+     eng,japn= list
+    hash[get_emoticon][eng] = japanese
+    hash[get_meaning][japn] = meaning
+ end
+ hash
 end
+
 
 
 
