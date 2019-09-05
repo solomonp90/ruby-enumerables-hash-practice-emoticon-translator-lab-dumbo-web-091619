1095f1d6bad27787f13d3b9c88a7d9aeee14dbce
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-05 23:49:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_japanese_emoticon(file,emoticon)
     hash_library["get_emoticon"][emoticon] :
     "Sorry, that emoticon was not found"
 end
-end
+
 
 def get_english_meaning
   # code goes here
